602b7ef92cb5f5fd2874b87e137ca2abb08690e0
gouththam <gouththam302@gmail.com>
2018-09-05 11:34:12 +0530

    this tester4

--- app/routes.js
+++ app/routes.js
@@ -1,46 +1,42 @@
-
 module.exports = function(app, passport) {
-  var getUrl = window.location;
-  var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-  console.log(baseUrl);
 
-    app.get(baseUrl, function(req, res) {
+    app.get('/', function(req, res) {
       res.render('index.ejs', {
         user : req.user
       });
     });
 
-    app.get(baseUrl + '/signup', function(req, res) {
+    app.get('/signup', function(req, res) {
       res.render('register.ejs', { message: req.flash('signupMessage') });
     });
 
-    app.post(baseUrl +'/signup', passport.authenticate('local-signup', {
+    app.post('/signup', passport.authenticate('local-signup', {
       successRedirect : '/',
       failureRedirect : '/signup',
       failureFlash : true
     }));
 
-    app.get(baseUrl + '/login', function(req, res) {
+    app.get('/login', function(req, res) {
         res.render('login.ejs', { message: req.flash('loginMessage')});
     });
     //
-    app.post(baseUrl +'/login', passport.authenticate('local-login', {
+    app.post('/login', passport.authenticate('local-login', {
         successRedirect : '/',
         failureRedirect : '/login',
         failureFlash : true
     }));
     //
-    app.get(baseUrl + '/connect/local', function(req, res) {
+    app.get('/connect/local', function(req, res) {
       res.render('connect-local.ejs', { message: req.flash('loginMessage')});
     });
     //
-    app.post(baseUrl +'/connect/local', passport.authenticate('local-signup', {
+    app.post('/connect/local', passport.authenticate('local-signup', {
       successRedirect : '/',
       failureRedirect : '/connect/local',
       failureFlash : true
     }));
     //
-    app.get(baseUrl + '/unlink/local', function(req, res) {
+    app.get('/unlink/local', function(req, res) {
       var user            = req.user;
       user.local.email    = undefined;
       user.local.password = undefined;
